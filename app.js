29bc651a4e9e73ef973bd08c63f53cacd69a5b38
lifeann <149606573+lifeann@users.noreply.github.com>
2024-04-22 09:00:48 +0400

    controllers

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
+const mainRoute = require('./routes/main');
+const gamesRouter = require('./routes/games'); 
 
 const PORT = 3000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+app.use(mainRoute, gamesRouter); 
 
 app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}`)
-})  
+  console.log(`Server is running at PORT http://localhost:${PORT}`);
+})
---
+++ routes/games.js
@@ -0,0 +1,8 @@
+const gamesRouter = require("express").Router(); 
+const {getAllGames, deleteGame,
+} = require("../controllers/games");
+
+gamesRouter.get("/games", getAllGames)
+gamesRouter.delete("/games/:id", deleteGame);
+
+module.exports = gamesRouter;
